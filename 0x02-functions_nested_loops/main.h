637364c1caf037ed5cb92a6cbc19af5f64e7d712
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-21 23:56:12 +0100

    fixing print_alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - contain function prototypes
+ * _putchar - prints charachter
+ * print_alphabet - print alphabets in lower case
  * Return: no return
  */
 
-int putchar (int character);
+int _putchar (int character);
 
 void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar (tolower(n));
+	putchar('\n');
+}
+
 
