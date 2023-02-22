2e0c1aec24dee897fcfe0fc9a8b337874e73dbad
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-22 02:19:51 +0100

    fixing 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include"main.h"
 
 /**
- * _islower - for each alphabet checks if it to a lower case
+ * _isalpha - for each alphabet checks if it to a lower case
  * @c: character entry
  * it executes, and prints it
  * Return: 1 if entry is lowercase 0 otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c < 65 || c > 122)
 		return (0);
