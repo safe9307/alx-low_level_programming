2cbd1fcf17a04e2065311bca4be28e35d4849d3f
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-22 02:56:32 +0100

    adding 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * _abs - absolute value of a number
+ * @n: number entry
+ * it executes, and prints it
+ * Return: 1 if pos, 0 if null, -1 if neg
+ */
+int _abs(int n)
+{
+	return (n * n);
+}
