8a062869e743d83ee5e52778e2c9e5f6529192bd
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-22 02:00:45 +0100

    adding 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _islower - for each alphabet checks if it to a lower case
+ * @c: character entry
+ * it executes, and prints it
+ * Return: 1 if entry is lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c < 97 || c > 122)
+		return (0);
+	else
+		return (1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
  * print_alphabet - print alphabets in lower case
  * character - phrase
  * print_alphabet_x10 - print alphabets in lower case 10 times
+ * _islower - return 1 if entry lowercase 0 otherwise
  * Return: no return
  */
 
@@ -11,3 +12,5 @@ int _putchar (int character);
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
+
+int _islower(int c);
