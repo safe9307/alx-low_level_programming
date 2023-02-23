d1d44bc4e8508a89632f2f91e2438b2ec1716968
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-23 23:30:33 +0100

    0-main.c debuggin

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include"main.h"
+
+/**
+ * main - test positive_or_negative() in case of 0
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	positive_or_negative(0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0-main.c test positive_or_negative(0). <br/ >
