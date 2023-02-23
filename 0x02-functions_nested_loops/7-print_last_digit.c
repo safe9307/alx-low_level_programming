6342951fb1993543f303afdea6ef3a0290acbd23
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-23 02:55:12 +0100

    fixing 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,8 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = _abs(n) % 10;
+	last_digit = n % 10;
+	last_digit = _abs(last_digit);
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
