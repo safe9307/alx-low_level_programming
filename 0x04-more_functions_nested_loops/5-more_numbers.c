0ba91d4f2a00903ffeff41ea11f43d753cd54360
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-25 00:20:15 +0100

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14, 10 times
+ * Return: the multiplication
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			_putchar(j + 48);
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,5 @@
 1-isdigit.c checks if entry is digit or not. <br/ >
 3-print_numbers.c prints numbers from 0 to 9. <br/ >
 4-print_most_numbers.c, prints numbers from 0 to 9 with the exception of 2 and 4 <br/ >
+5-more_numbers.c , prints 10 time numbers from 0 to 14 <br/ >
+
