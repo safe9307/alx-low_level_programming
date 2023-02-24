3a09768a6605ac95b3e654f4cfc515a921680666
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-24 01:14:55 +0100

    adding 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * _isupper - checks if entry is digit or not
+ * _isdigit - checks if entry is digit or not
  * @c: entry character
  * Return: 1 if c is digit 0 if not
  */
