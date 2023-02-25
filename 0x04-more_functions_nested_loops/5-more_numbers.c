c2117b8c8753e359c645760c467dab28213110f1
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-25 02:33:15 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,11 @@ void more_numbers(void)
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
+		{
 			if (j / 10 > 0)
 				_putchar((j / 10) + '0');
 			_putchar((j % 10) + 48);
-		_putchar('\n');
+			_putchar('\n');
+		}
 	}
 }
