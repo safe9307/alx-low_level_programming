4378bcea1b979b6b1a15c3f49a57783ca855385b
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-27 22:50:04 +0100

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * reset_to_98 - updates the value the pointer points to, to 98
+ * @n: variable of pointer *n
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar (int character);
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
