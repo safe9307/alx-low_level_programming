68091c678f1affa8828ceccc9aa23d051641795c
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-27 23:47:49 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include"2-strlen.c"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line to stdout
+ * @s: string variable of pointer *s
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int i, len = _strlen(*s);
+
+	for (i = len - 1; i >= len; i--)
+		_putchar(s[len]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1-swap.c swaps the value of 2 integers. <br/ >
 2-strlen.c returns the length of a string. <br/ >
 3-puts.c prints a string, followed by a new line, to stdout. <br/ >
+4-print_rev.c prints a string in reverse. <br/ >
