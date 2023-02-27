94b3ae3138f754ce507e828b5a6f39142b35a5f8
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-27 22:55:57 +0100

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: variable of pointer *a
+ * @b: variable of pointer *b
+ * Return: no return
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *x;
+	*x = *a;
+	*a = *b;
+	*x = *x
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,6 @@ int _putchar (int character);
 
 void reset_to_98(int *n);
 
+void swap_int(int *a, int *b);
+
 #endif /* MAIN_H */
