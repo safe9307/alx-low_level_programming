775c2b50abf3059dbaa6cc904cf537dc19707d60
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-02-28 02:49:24 +0100

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,10 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	if (n == (sizeof(*a) / 4))
+	while (i < (n - 1))
 	{
-		while (i < (n - 1))
-		{
-			printf("%d, ", a[i]);
-			i++;
-		}
-		printf("%d\n", a[n - 1]);
+		printf("%d, ", a[i]);
+		i++;
 	}
+	printf("%d\n", a[n - 1]);
 }
