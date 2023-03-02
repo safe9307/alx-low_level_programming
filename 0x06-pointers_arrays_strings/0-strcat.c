f8453569a1e582024ed5899f6e063fe43b6b85ed
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-03-02 23:49:00 +0100

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: string appended to dest
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len_dest = 0, len_src = 0;
+
+	while (dest[len_dest] != '\0')
+		len_dest++;
+	while (src[i] != '\0')
+	{
+		dest[len_dest + len_src] = src[len_src];
+		len_src++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar (int character);
+
+int _putchar (int character);
+
+#endif /* MAIN_H */
