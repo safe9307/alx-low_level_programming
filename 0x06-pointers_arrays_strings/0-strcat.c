ddbd5ec63f8445f15563b0121a2ec81933c59ad3
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-03-02 23:55:32 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,7 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[len_dest] != '\0')
 		len_dest++;
-	while (src[i] != '\0')
+	while (src[len_src] != '\0')
 	{
 		dest[len_dest + len_src] = src[len_src];
 		len_src++;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@
 
 int _putchar (int character);
 
-int _putchar (int character);
+char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */
