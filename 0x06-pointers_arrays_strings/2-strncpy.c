6b37775530bc1c636fc5b3545dd88bf98a1c3f44
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-03-03 01:29:08 +0100

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: copied string
+ * @src: string to copy
+ * @n: maximun n bytes from src
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int len = 0;
+
+	while (src[len] != src[n])
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-strcat.c concatenates two strings. <br/ >
 1-strncat.c concatenates two strings with maximum n bytes from src. <br/ >
+2-strncpy.c copies a string. <br/ >
