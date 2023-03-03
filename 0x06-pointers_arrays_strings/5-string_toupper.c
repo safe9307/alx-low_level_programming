c2a22ff39074a0576584d671384da66ffa8e706d
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-03-03 03:52:44 +0100

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,12 @@
 
 char *string_toupper(char *a)
 {
-	int len = 0, temp;
+	int len = 0;
 
 	while (a[len] != '\0')
 	{
-		if (a[len] >= 65 && a[len] <= 90)
-			a[len] = a[len] + 32;
+		if (a[len] >= 97 && a[len] <= 122)
+			a[len] = a[len] - 32;
 	}
 	return (a);
 }
