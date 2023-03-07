901edb72e9c5e77b61085079f0993c0c8645f2c5
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-03-07 02:10:14 +0100

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a charachter in a string
+ * @s: source string
+ * @c: character to locate
+ * Return: pointer to 1st occurrence of c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int len = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[len] == c)
+			return (s + len);
+		len++;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-memset.c fills memory with a constant byte. <br/ >
 1-memcpy.c copies memory area. <br/ >
+2-strchr.c locates a charachter in a string. <br/ >
