69a06448abdf1c0a4c6a5e2ae7cff30597d12452
Amina BOUHAMRA <bouhamraamina@gmail.com>
2023-03-09 00:52:00 +0100

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _puts_recursion - prints a string.
+ * @s: entry string
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		_putchar(s[i]);
+}
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int character);
+
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
